fe6adbf1db5c60fa24fa28f1ac6012ff31a15f98
fadillahazhardk <gamaisacademy@gmail.com>
2021-04-22 23:09:39 +0700

    serving static file

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
+const path = require("path");
+
+//REGISTER PLUGIN
+fastify.register(require("fastify-static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
 
 // Declare a route
-fastify.get("/", async (request, reply) => {
+
+//SSR
+fastify.get("/", function (req, reply) {
+  return reply.sendFile("index.html"); // serving path.join(__dirname, 'public', 'index.html') directly
+});
+
+//API
+fastify.get("/api", async (request, reply) => {
   return { hello: "world" };
 });
 
