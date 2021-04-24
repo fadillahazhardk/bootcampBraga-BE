16d4004e1948432e9b7280810d45b02a51f06df4
fadillahazhardk <gamaisacademy@gmail.com>
2021-04-24 07:14:58 +0700

    fix heroku config

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ fastify.get("/api", async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT);
+    await fastify.listen(process.env.PORT, "0.0.0.0");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
